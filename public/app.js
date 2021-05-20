0b62a34b605618015cfa0e8ec2b2367dd161336c
Pradhumn Sharma <chris.m.labiak@gmail.com>
2021-05-20 12:48:36 +0530

    Added spinner when forecast load.

--- public/app.js
+++ public/app.js
@@ -1,11 +1,21 @@
-// document.getElementById('main').innerHTML = '';
-
 const form = document.querySelector('form');
 const searchElement = document.querySelector('input');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  fetch('http://localhost:3000/weather?address=' + searchElement.value)
-    .then(response => response.json()
-      .then(data => document.getElementById('main').innerHTML = data.data))
+  if(searchElement.value) {
+    document.getElementById('main').style.display = 'none';
+    document.getElementsByClassName('loader')[0].classList.remove('display-none');
+    fetch('http://localhost:3000/weather?address=' + searchElement.value)
+      .then(response => response.json()
+        .then(data => {
+          document.getElementById('main').innerHTML = data.data;
+          document.getElementById('main').classList.remove('warning');
+          document.getElementById('main').style.display = 'block';
+          document.getElementsByClassName('loader')[0].classList.add('display-none');
+        }))
+  } else {
+    document.getElementById('main').classList.add('warning');
+    document.getElementById('main').innerHTML = 'Please enter correct address';
+  }
 })
